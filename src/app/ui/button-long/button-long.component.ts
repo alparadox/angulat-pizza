bb1c6e5a6420d7128995606751a81abf31e5a64c
Alyona <alyona.dux@yandex.ru>
2023-01-27 15:33:41 +0300

    Добавлены 2 кнопки, отрефакторины стили

--- src/app/ui/button-long/button-long.component.ts
+++ src/app/ui/button-long/button-long.component.ts
@@ -9,13 +9,19 @@ export class ButtonLongComponent implements OnInit {
 
   @Input() arrowIsShow: boolean = false;
   @Input() textOnButton: string = 'Вернуться назад';
-  @Input() colorOrange: boolean = false;
-  @Input() colorBlack: boolean = false;
-  @Input() colorWhite: boolean = false;
+  @Input() type: 'primary' | 'secondary' | 'outline' = 'primary';
+  iconSrc: string = 'assets/img/grey-arrow-left.svg';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeIcon(): void {
+    this.iconSrc = 'assets/img/grey-arrow-left-hover.svg'
+  }
+
+  changeIconBack(): void {
+    this.iconSrc = 'assets/img/grey-arrow-left.svg'
+  }
 }
