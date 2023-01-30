a2c543ec33b3869a92df98c014319c9fe93ad01a
Alyona <alyona.dux@yandex.ru>
2023-01-30 10:12:16 +0300

    Добавлены круглые кнопки

--- src/app/ui/button-long/button-long.component.ts
+++ src/app/ui/button-long/button-long.component.ts
@@ -8,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ButtonLongComponent implements OnInit {
 
   @Input() textOnButton: string = 'Вернуться назад';
-  @Input() type: 'primary' | 'secondary' | 'outline' | 'ghost' = 'primary';
+  @Input() type: 'primary' | 'secondary' | 'outline' | 'ghost' | 'circle' | 'circle-delete'= 'primary';
   iconSrc: string = '';
   arrowIsShow: boolean = false;
   basketIsShow: boolean = false;
