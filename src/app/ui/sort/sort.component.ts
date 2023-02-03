b21e30b04ab5aabcbb8131e9233fc48cf25ccec0
Alyona <alyona.dux@yandex.ru>
2023-02-03 12:05:57 +0300

    сортировка wip

--- src/app/ui/sort/sort.component.ts
+++ src/app/ui/sort/sort.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SortComponent implements OnInit {
 
+  sort: any = ["популярности", "цене", "алфавиту"];
+
   constructor() { }
 
   ngOnInit(): void {
