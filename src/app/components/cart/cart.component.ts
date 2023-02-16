0b5ad5569c6977497727e39843c0d956d9c6a7b3
Alyona <alyona.dux@yandex.ru>
2023-02-16 16:18:44 +0300

    сделана верстка корзины

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {IPizza} from "../../interfaces/pizza";
+import {PizzaService} from "../../pizza.service";
 
 @Component({
   selector: 'app-cart',
@@ -7,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  items$: Observable<IPizza[]>;
+  productCount: number = 0;
+  productSum: number = 0;
+
+  constructor(
+    private pizza: PizzaService
+  ) {
+    this.items$ = this.pizza.getPizzas();
+  }
 
   ngOnInit(): void {
   }
